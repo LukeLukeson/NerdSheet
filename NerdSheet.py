dd0d609ae761fff667955c53fb02ca8daecb2574
SyntaxError528 <legoman@myfairpoint.net>
2018-11-06 16:46:42 -0500

    Added Dice rolling

--- NerdSheet.py
+++ NerdSheet.py
@@ -1,4 +1,19 @@
 # Made by Lucien Hammond
 # Using MatthewGallant's PyDB for saving variables
 import tkinter, sys, time, random, os
-import PyDB as db
+
+def rollTwenty(abilityScore):
+    twenty = random.randint(1,20)
+    if twenty == 20:
+        print('Natural 20')
+    elif twenty == 1:
+        print('Critical Fail')
+    else:
+        twentyFinal = twenty + int(abilityScore)
+        print(str(twenty) + ' + ' + str(abilityScore) + ' = ' + str(twentyFinal)) 
+
+
+    
+while True:
+    abilityScore = input('Enter ability score: ')
+    rollTwenty(abilityScore)
