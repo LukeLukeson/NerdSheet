1d3d8f66d46ced055bd70409e2286f4d42158ee8
SyntaxError528 <legoman@myfairpoint.net>
2018-11-06 17:33:18 -0500

    Setup Structure

--- NerdSheet.py
+++ NerdSheet.py
@@ -1,6 +1,9 @@
 # Made by Lucien Hammond
 # Using MatthewGallant's PyDB for saving variables
+
 import tkinter, sys, time, random, os
+
+# Rolls a twenty sided die
 
 def rollTwenty(abilityScore):
     twenty = random.randint(1,20)
@@ -12,8 +15,37 @@ def rollTwenty(abilityScore):
         twentyFinal = twenty + int(abilityScore)
         print(str(twenty) + ' + ' + str(abilityScore) + ' = ' + str(twentyFinal)) 
 
+# Rolls a twelve sided die
 
+def rollTwelve(abilityScore):
+    twelve = random.randint(1,12)
+    twelveFinal = twelve + int(abilityScore)
+    print(str(twelve) + ' + ' + str(abilityScore) + ' = ' + str(twelveFinal))
+
+def rollTen():
+    pass
+
+def rollEight():
+    pass
+
+def rollSix():
+    pass
+
+def rollFour():
+    pass
+
+def rollHundred():
+    pass
     
+# Main Loop
+
 while True:
     abilityScore = input('Enter ability score: ')
-    rollTwenty(abilityScore)
+
+
+
+    if int(abilityScore) > -11 and int(abilityScore) < 11:
+        rollTwenty(abilityScore)
+    else:
+        print('Thats not a valid ability score')
+    
