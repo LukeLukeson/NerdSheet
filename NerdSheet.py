ca95b05f9587ec855d628233937cfd13120a626d
SyntaxError528 <legoman@myfairpoint.net>
2018-11-11 12:21:31 -0500

    d20 and d12 rolling support

--- NerdSheet.py
+++ NerdSheet.py
@@ -1,51 +1,22 @@
 # Made by Lucien Hammond
-# Using MatthewGallant's PyDB for saving variables
 
-import tkinter, sys, time, random, os
+import sys, time, random, os
+from dice import dice
 
-# Rolls a twenty sided die
 
-def rollTwenty(abilityScore):
-    twenty = random.randint(1,20)
-    if twenty == 20:
-        print('Natural 20')
-    elif twenty == 1:
-        print('Critical Fail')
-    else:
-        twentyFinal = twenty + int(abilityScore)
-        print(str(twenty) + ' + ' + str(abilityScore) + ' = ' + str(twentyFinal)) 
 
-# Rolls a twelve sided die
+die = dice()
 
-def rollTwelve(abilityScore):
-    twelve = random.randint(1,12)
-    twelveFinal = twelve + int(abilityScore)
-    print(str(twelve) + ' + ' + str(abilityScore) + ' = ' + str(twelveFinal))
-
-def rollTen():
-    pass
-
-def rollEight():
-    pass
-
-def rollSix():
-    pass
-
-def rollFour():
-    pass
-
-def rollHundred():
-    pass
-    
 # Main Loop
 
 while True:
-    abilityScore = input('Enter ability score: ')
-
-
-
-    if int(abilityScore) > -11 and int(abilityScore) < 11:
-        rollTwenty(abilityScore)
+    x = input('Enter ability score want: ')
+    y = input('What die do you  to roll?: ')
+    
+    if int(x) > -20 and int(x) < 20:
+        die.roll(int(y), int(x))
     else:
-        print('Thats not a valid ability score')
+        print('What are you doing that requires you to have that as an ability score?!?!?!?')
+        time.sleep(1)
+
     
---
+++ dice.py
@@ -0,0 +1,25 @@
+import random
+
+class dice:
+
+    def roll(self, type, abilityScore):
+        
+        self.type = type
+        self.abilityScore = abilityScore
+
+        if type == 20:
+            
+            twenty = random.randint(1,20)
+            if twenty == 20:
+                print('Natural 20')
+            elif twenty == 1:
+                print('Critical Fail')
+            else:
+                twentyFinal = twenty + int(abilityScore)
+                print(str(twenty) + ' + ' + str(abilityScore) + ' = ' + str(twentyFinal)) 
+
+        elif type == 12:
+
+            twelve = random.randint(1,12)
+            twelvefinal = twelve + int(abilityScore)
+            print(str(twelve) + ' + ' + str(abilityScore) + ' = ' + str(twelvefinal))
